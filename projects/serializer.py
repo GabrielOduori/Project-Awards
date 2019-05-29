d6f245eb99d31811e4971ce4c34c28887e2def46
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-29 13:12:46 +0300

    Serializers
    Serializer file is responsible for making our data available to external users.

---
+++ projects/serializer.py
@@ -0,0 +1,31 @@
+from accounts.models import Profile
+from projects.models import Project
+
+from rest_framework import serializers
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
+        fields = (
+            'user',
+            'bio',
+            'email'
+        )
+        
+        
+        
+        
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Project
+        fields = (
+            'title',
+            'description', 
+            'profile', 
+            'pub_date',
+            'website'
+        )
