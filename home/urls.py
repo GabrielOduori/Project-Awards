7664b63e1b80eb06a57cbef4d8bd407206fbde24
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-29 23:04:18 +0300

    Removed extra lines in the code.

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,12 @@ from projects import views as project_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     url(r'^$', home_views.home, name = 'home-page'),
     url(r'^api/profile/$', project_views.ProfileAPI.as_view()),
     url(r'^api/project/$', project_views.ProjectAPI.as_view()),
+   
       
     
 ]
