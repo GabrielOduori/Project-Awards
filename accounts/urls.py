3bc0cee2bd3bedc52465e80a71c7094b9e4cfe67
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-29 12:14:54 +0300

    Removed an extra line in the accounst app urls configuration file.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login,logout
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     
     url(r'^$', views.home, name='home'),
     url(r'^login/$', login, {'template_name':'accounts/login.html'}),
-    url(r'^login/$', login, {'template_name':'accounts/login.html'}),
     url(r'^register/$',views.register, name = 'register'),
     url(r'^profile/$', views.view_profile, name = 'view_profile' ),
     url(r'^profile/edit/$', views.edit_profile, name = 'edit_profile' ),
     url(r'^change-password/$', views.change_password, name = 'change_password' )
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
