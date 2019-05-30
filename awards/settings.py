ef8933a6066241f94cd754bc8b719a4a0af7b6bd
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-30 12:34:32 +0300

    Added django context-processor to settings.

--- awards/settings.py
+++ awards/settings.py
@@ -74,6 +74,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'django.template.context_processors.media',
+                
             ],
         },
     },
@@ -180,12 +182,12 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-LOGIN_REDIRECT_URL =  '/accounts/'
+
+LOGIN_REDIRECT_URL =  '/accounts/profile/'
 # LOGIN_REDIRECT_URL =  '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
